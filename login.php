820443ff09693398960695721ab8b057fec32445
visitor-4137 <62020063+visitor-4137@users.noreply.github.com>
2020-03-12 22:36:37 +0530

    Add files via upload
    Mene login or registration page ka kam pura kar diya h.. Check kr lena or agar suggestion ho to bata dena

--- login.php
+++ login.php
@@ -1,9 +1,58 @@
 <?php 
-	$username = "Username"
-	$password = "password"
-    $db = mysql_connect($username,$password);
-    if($db){
-
+    $conn = mysqli_connect("localhost","root","");
+    mysqli_select_db($conn,"logins");
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+       $username = $_POST['username'];
+       $password = $_POST['password'];
+       $username = stripcslashes($username);
+       $password = stripcslashes($password);
+       $username = mysqli_real_escape_string($conn,$username);
+       $password = mysqli_real_escape_string($conn,$password);
+       // to prevent mysqli injection
+       $pass = md5($password);
+       $query = "select * from logininfo where username='".$username."' and password='".$pass."'";
+       $result = mysqli_query($conn,$query);
+       $count = mysqli_num_rows($result);
+       if($count == 1){
+        $user = mysqli_fetch_assoc($result);
+        if($user['role'] === "Manager"){
+          header('location:registration.php');
+        }
+        else if($user['role'] === "TA"){
+          header('location:adminpro.html');
+        }
+       }
+       else{
+         echo  '<div class="alert alert-danger">
+                <a href="login.php" class="close" data-dismiss="alert" aria-label="close">Close X</a>
+                <p><strong>Alerta!</strong></p>
+                Username or password wrong! Please try again!.
+            </div>';
+       }
     }
     $conn -> close();
  ?>
+
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <title>Login Form</title>
+  <link rel='stylesheet' href='https://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
+  <link rel="stylesheet" href="css/login.css">
+
+</head>
+<body>
+
+<div class="wrapper">
+    <form class="form-signin" action="" method = "post">       
+      <h2 class="form-signin-heading">Login</h2>
+      <input type="text" class="form-control" name="username" placeholder="Username" required="" autofocus="" />
+      <input type="password" class="form-control" name="password" placeholder="Password" required=""/> 
+      </label>
+      <button class="btn btn-lg btn-primary btn-block" name = "submit" type="submit">Login</button>   
+    </form>
+  </div>
+</body>
+
+</head>
--- registration.php
+++ registration.php
@@ -1,3 +1,37 @@
+<?php 
+
+$servername = "localhost";
+$conn = mysqli_connect($servername,"root","");
+mysqli_select_db($conn,"logins");
+if(isset($_POST['reg_user'])){
+    $role = mysqli_real_escape_string($conn,$_POST['role']);
+    $user = mysqli_real_escape_string($conn,$_POST['username']);
+    $pass = mysqli_real_escape_string($conn,$_POST['password']);
+    $cpass = mysqli_real_escape_string($conn,$_POST['cpassword']);
+    if($pass != $cpass){
+        die();
+    }
+    $sql = "select * from logininfo where username='".$user."' limit 1";
+    $result = mysqli_query($conn,$sql);
+    $user1 = mysqli_fetch_assoc($result);
+    if($user1){
+        if($user1['username'] === $user){
+            echo  '<div class="alert alert-danger">
+                <a href="registration.php" class="close" data-dismiss="alert" aria-label="close">Close X</a>
+                <p><strong>Alert!</strong></p>
+                Username already exists! Please try again!.
+            </div>';
+        }
+    }
+    $password = md5($pass);
+    $query = "INSERT INTO logininfo (role,username,password) VALUES('$role','$user','$password')";
+    $result2 = mysqli_query($conn,$query);
+    if($result2){
+     header('location: registration.php');
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,44 +51,12 @@
       </select>
       <br>
       <br>
-      <input type="text" class="form-control" name="username" placeholder="username"><br>
+      <input type="text" class="form-control" name="username" placeholder="Username">
       <br>
-        <input type="password" class="form-control" name = "password" placeholder="password"><br>
-      
-         <input type="password" class="form-control" name="cpassword" placeholder="Confirm Password"><br><br>
+      <input type="password" class="form-control" name = "password" placeholder="Password">
+      <input type="password" class="form-control" name="cpassword" placeholder="Confirm Password">
       <button name="reg_user" class="btn btn-lg btn-primary btn-block" type = "submit">SUBMIT</button>
     </form>
     </div>
 </body>
 </html>
-
-
-<?php 
-
-$servername = "localhost";
-$conn = mysqli_connect($servername,"root","");
-mysqli_select_db($conn,"testlog2");
-if(isset($_POST['reg_user'])){
-    $role = mysqli_real_escape_string($conn,$_POST['role']);
-    $user = mysqli_real_escape_string($conn,$_POST['username']);
-    $pass = mysqli_real_escape_string($conn,$_POST['password']);
-    $cpass = mysqli_real_escape_string($conn,$_POST['cpassword']);
-    if($pass != $cpass){
-        die();
-    }
-    $sql = "select * from logininfo where username='".$user."' limit 1";
-    $result = mysqli_query($conn,$sql);
-    $user1 = mysqli_fetch_assoc($result);
-    if($user1){
-        if($user1['username'] === $user AND $user1['role'] === $role){
-            die();
-        }
-    }
-    $password = md5($pass);
-    $query = "INSERT INTO logininfo (role,username,password) VALUES('$role','$user','$password')";
-    $result2 = mysqli_query($conn,$query);
-    if($result2){
-     header('location: registration.php');
-    }
-}
-?>
