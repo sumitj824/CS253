6d56f3b3c67ecd8bd9c96b60a2b54a19281e3a45
visitor-4137 <62020063+visitor-4137@users.noreply.github.com>
2020-03-14 22:38:29 +0530

    Add files via upload
    naya dashboard wala page h

---
+++ dashboard.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="css/dashboard.css">
+</head>
+<body>
+	<div class = "box">
+    	<div class = "heading">Dashboard</div>
+    	<hr>
+		<div class = "first">
+			<a href="registration.php">Register TA/Manager</a>
+		</div>
+		<hr>
+		<div class = "second">
+			<a href="#">Distribute Applications</a>
+		</div>
+		<hr>
+		<div class = "third">
+			<a href="changeview.php">View a Member</a>
+		</div>
+		<hr>
+		<div class = "fourth">
+			<a href="#">Queries from Manager</a>
+		</div>
+	</div>
+	<div class = "main-space2">
+		<iframe type="application/pdf" src="sql.pdf" width="100%" height="100%" align="right"></iframe>
+	</div>
+	<div class = "main-space">
+		<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTmn--p-ZKw4LNWI1MCr-ivFkIfaFpMpEjxoAonTKT3S0ITue38FqNY7AfSFKxeQeJphjRmnWYZROPG/pubhtml?gid=856111278&amp;single=true&amp;widget=true&amp;headers=false" width = "100%" height = "100%" align = "right"></iframe>
+	</div>
+</body>
+</html>
