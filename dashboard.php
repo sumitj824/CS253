40c83b1580ab897b1aa9d8837e8f181390c2636a
visitor-4137 <62020063+visitor-4137@users.noreply.github.com>
2020-03-23 21:41:10 +0530

    Add files via upload
    necessary changes to dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,14 +2,105 @@
 <html>
 <head>
 	<title>Dashboard</title>
-	<link rel="stylesheet" type="text/css" href="css/dashboard.css">
+	<link rel='stylesheet' href='https://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css'>
 </head>
+<style type="text/css">
+	.box {
+	font-family : Calibri;
+	position : fixed;
+	width : 18vw;
+	height : 100vh;
+    background-color : white;
+    top : 0px;
+    left : 0px;
+	border-right : 4px solid black;
+}
+
+.heading{
+	background : ;
+	margin : auto;
+	padding : 1vw;
+	font-size : 3vw;
+	text-align : center;
+	color : blue;
+}
+
+
+.first {
+	background : white;
+	margin : auto;
+	padding : 1vw;font-size : 1.5vw;
+	text-align : center;
+	color : blue;
+}
+
+.second {
+	background : white;
+	margin : auto;
+	padding : 1vw;font-size : 1.5vw;
+	text-align : center;
+	margin : auto;
+}
+
+.third{
+	background : white;
+	padding : 1vw;
+	margin  : auto;font-size : 1.5vw;
+	text-align : center;
+}
+
+.fourth{
+	margin : auto;
+	background : white;
+	padding : 1vw;font-size : 1.5vw;
+	text-align : center;
+}
+
+.main-space{
+	position : absolute;
+	left : 15.0vw;
+	width: 1300px;
+	top : 0px;
+	height : 720px;
+	color : blue;
+}
+
+.main-space2{
+	position : relative;
+	font-size : 40px;
+	display : block;
+	top : 30%;
+	text-align : center;
+	color : cyan;
+}
+
+a {
+	display : block;
+	color : blue;
+	padding : 0.2vw;
+	text-decoration: none;
+}
+
+a:hover{
+	color : red;
+	text-decoration : none;
+}
+
+@media screen and(max-width:1200px){
+    .box{
+    	font : 10px;
+    }
+}        
+</style>
 <body>
+	<div class = "main-space">
+	    <div class = "main-space2">Welcome to Electrical Department, IIT Kanpur</div>
+	</div>
 	<div class = "box">
-    	<div class = "heading">Dashboard</div>
+    	<div class = "heading"><a href = "dashboard.php">Dashboard</a></div>
     	<hr>
 		<div class = "first">
-			<a href="registration.php">Register TA/Manager</a>
+			<a href="dashboardregister.php">Register TA/Manager</a>
 		</div>
 		<hr>
 		<div class = "second">
@@ -23,12 +114,6 @@
 		<div class = "fourth">
 			<a href="#">Queries from Manager</a>
 		</div>
-	</div>
-	<div class = "main-space2">
-		<iframe type="application/pdf" src="sql.pdf" width="100%" height="100%" align="right"></iframe>
-	</div>
-	<div class = "main-space">
-		<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTmn--p-ZKw4LNWI1MCr-ivFkIfaFpMpEjxoAonTKT3S0ITue38FqNY7AfSFKxeQeJphjRmnWYZROPG/pubhtml?gid=856111278&amp;single=true&amp;widget=true&amp;headers=false" width = "100%" height = "100%" align = "right"></iframe>
 	</div>
 </body>
 </html>
